70a04e39b4fcf2f085eb3ba9b9266ef4ed7f88c3
Himmler Louissaint <hlouissaint@hotmail.com>
2022-07-06 20:16:39 -0400

    controlled search component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Header } from './components/Header';
+import { Search } from './components/Search';
+import { useState } from 'react';
+
 
 
 const App = () => {
+  const [imgName, setImgName] = useState('');
+  useState('')
+  
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(imgName);
+  }
+
+  
   return (
     <div>
       <Header title="Images Gallery"/>
+      <Search handleSubmit={handleSearchSubmit} imgName={imgName} setImgName={setImgName}/>
     </div>
   );
 }
