3b1d82ebadba295603adc4d8d162718fcbe55f84
Himmler Louissaint <hlouissaint@hotmail.com>
2022-07-14 14:18:54 -0400

    Welcome component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Header } from "./components/Header";
 import { Search } from "./components/Search";
 import { ImageCard } from "./components/ImageCard";
+import { Welcome } from "./components/Welcome";
 import { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 
@@ -39,13 +40,17 @@ function App() {
         setImgName={setImgName}
       />
       <Container className="mt-4">
-        <Row xs={1} md={2} lg={3}>
-          {images.map((image, i) => (
-            <Col key={i} className="pb-3">
-              <ImageCard image={image} deleteImage={handleDeleteImage} />
-            </Col>
-          ))}
-        </Row>
+        {images.length ? (
+          <Row xs={1} md={2} lg={3}>
+            {images.map((image, i) => (
+              <Col key={i} className="pb-3">
+                <ImageCard image={image} deleteImage={handleDeleteImage} />
+              </Col>
+            ))}
+          </Row>
+        ) : (
+          <Welcome />
+        )}
       </Container>
     </div>
   );
